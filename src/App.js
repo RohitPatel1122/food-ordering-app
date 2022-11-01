0190538f7359ce9e2104d0da8eb0fe3caed30f4c
Rohit Patel <rohitpatel.1122@gmail.com>
2022-11-01 13:51:17 +0530

    Added meals section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/header/Header";
+
 import React from "react";
+import Meals from "./components/meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </React.Fragment>
   );
 }
